855383bda09fd3cdd95aec1a8cadd22606dcc5c3
Max Melcher <mmelcher@alegri.eu>
2018-06-26 18:37:41 +0200

    initial

--- AzureSaturday/Controllers/HomeController.cs
+++ AzureSaturday/Controllers/HomeController.cs
@@ -17,15 +17,16 @@ namespace AzureSaturday.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
 
+        public IActionResult FAQ()
+        {
             return View();
         }
 
---
+++ AzureSaturday/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureSaturday.Helpers
+{
+    public static class Constants
+    {
+        public const string Brand = "AzureSaturday.org";
+        public const string Event = "AzureSaturday";
+    }
+}
